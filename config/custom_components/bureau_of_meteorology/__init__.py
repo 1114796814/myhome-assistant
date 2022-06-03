a46832c56c00979f8f26f3b3bb34a74402d6362a
1114796814 <1114796814@qq.com>
2022-06-03 21:57:10 +0800

    'update'

---
+++ config/custom_components/bureau_of_meteorology/__init__.py
@@ -0,0 +1,103 @@
+"""The BOM integration."""
+import asyncio
+import datetime
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import debounce
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from aiohttp.client_exceptions import ClientConnectorError
+
+from .PyBoM.collector import Collector
+from .const import (CONF_WEATHER_NAME,
+                    CONF_FORECASTS_BASENAME,
+                    DOMAIN, COLLECTOR, COORDINATOR)
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = ["sensor", "weather"]
+
+DEFAULT_SCAN_INTERVAL = datetime.timedelta(minutes=10)
+DEBOUNCE_TIME = 60  # in seconds
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the BOM component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_migrate_entry(hass, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+
+        new = {**config_entry.data}
+        if CONF_FORECASTS_BASENAME in new:
+            new[CONF_WEATHER_NAME] = config_entry.data[CONF_FORECASTS_BASENAME]
+
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry, data=new)
+
+    _LOGGER.info("Migration to version %s successful", config_entry.version)
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up BOM from a config entry."""
+    collector = Collector(
+        entry.data[CONF_LATITUDE],
+        entry.data[CONF_LONGITUDE]
+    )
+
+    try:
+        await collector.async_update()
+    except ClientConnectorError as ex:
+        raise ConfigEntryNotReady from ex
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=f"BOM observation {entry.data[CONF_WEATHER_NAME]}",
+        update_method=collector.async_update,
+        update_interval=DEFAULT_SCAN_INTERVAL,
+        request_refresh_debouncer=debounce.Debouncer(
+            hass, _LOGGER, cooldown=DEBOUNCE_TIME, immediate=True
+        )
+    )
+
+    await coordinator.async_refresh()
+
+    hass_data = hass.data.setdefault(DOMAIN, {})
+    hass_data[entry.entry_id] = {
+        COLLECTOR: collector,
+        COORDINATOR: coordinator,
+    }
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ readme.md
@@ -0,0 +1,5 @@
+### 挂载文件 config
+
+### 演示视频 demo
+
+### docker镜像 registry.cn-shanghai.aliyuncs.com/lianger/myhome-assistant:1.0
